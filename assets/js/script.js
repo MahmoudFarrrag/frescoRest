aecf3fefb63fd3a2b8f6160b00687a4d281b6d2c
MahmoudFarrrag <149093239+MahmoudFarrrag@users.noreply.github.com>
2024-06-16 18:51:13 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () { navbar.classList.toggle("active"); }
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () { navbar.classList.remove("active"); }
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+function sendToWhatsApp(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var emailAddress = document.getElementById('email_address').value;
+  var whatsappNumber = '+2001210855655'; // Replace this with your WhatsApp number
+  var whatsappLink = 'https://api.whatsapp.com/send?phone=' + encodeURIComponent(whatsappNumber) + '&text=' + encodeURIComponent(emailAddress);
+
+  // Open WhatsApp link
+  window.open(whatsappLink, '_blank');}
